f09deb973f9a333fd5aea19b7014f6525bfd09b7
ortiesk <ibrahimgunlu25@gmail.com>
2026-04-02 23:34:39 +0300

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  try {
+    const GAS_URL = "https://script.googleusercontent.com/macros/s/AKfycbx_N7t-dJSrmzOAv4TnEuarKKOYluMZlX7qf30gPAgOiK5vVChMVEWV8Ugunih9rNjAoQ/exec";
+
+    await fetch(GAS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    return res.status(200).send("OK");
+
+  } catch (error) {
+    return res.status(200).send("OK");
+  }
+}
